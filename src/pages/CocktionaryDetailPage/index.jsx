ac3f8536a1c31716a1e85d052992ea2eab74d511
Henry-Hong <hhr8540@naver.com>
2022-08-18 18:02:05 +0900

    칵셔너리 디테일 페이지 여기서 뭐 좀만 수정하면 좋을것같은뎁!

--- src/pages/CocktionaryDetailPage/index.jsx
+++ src/pages/CocktionaryDetailPage/index.jsx
@@ -3,22 +3,22 @@ import { useEffect, useState } from "react";
 import * as S from "./style";
 
 import { getCocktionaryCocktailDetails } from "../../apis/getCocktionaryCocktailDetails";
+import FullImg from "../../components/molecules/FullImg";
+import { Badge, Space, Text } from "@mantine/core";
 
 // GET 메인페이지 처음으로 할때
-const dummyDidMount = [
-  {
-    cocktail_id: "1",
-    cocktail_order_img: "", // 주문전용 칵테일 디비
-    cocktail_korname: "모히또",
-    cocktail_engname: "Mojito",
-    cocktail_alchol_degree: "15",
-    cocktail_description:
-      "모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는\n\n럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기\n\n럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기",
-    cocktail_ingredients: ["데낄라", "오렌지쥬스", "석류시럽"],
-    cocktail_price: "10000",
-    cocktail_img_for_order: "",
-  },
-];
+const dummyDidMount = {
+  cocktail_id: "1",
+  cocktail_order_img: "", // 주문전용 칵테일 디비
+  cocktail_korname: "모히또",
+  cocktail_engname: "Mojito",
+  cocktail_alchol_degree: "15",
+  cocktail_description:
+    "모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는\n\n럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기\n\n럼 기반 머시기 머시기 모히또는 럼 기반 머시기 머시기",
+  cocktail_ingredients: ["데낄라", "오렌지쥬스", "석류시럽"],
+  cocktail_price: "10000",
+  cocktail_img_for_order: "",
+};
 
 const CocktionaryDetailPage = () => {
   const [cocktails, setCocktails] = useState(dummyDidMount);
@@ -32,7 +32,30 @@ const CocktionaryDetailPage = () => {
 
   return (
     <>
-      <div>This is Cocktionary Detail Page</div>
+      <FullImg img={dummyDidMount.cocktail_img_for_order}></FullImg>
+      <Space h="xl"></Space>
+      <Text color="var(--primary-color)" size="2.5em">
+        {dummyDidMount.cocktail_engname}
+      </Text>
+      <S.RowWrapper>
+        {dummyDidMount.cocktail_ingredients.map((ingredient) => {
+          return (
+            <Badge style={{ margin: "0.5em 0.6em 0 0" }} color="violet">
+              {ingredient}
+            </Badge>
+          );
+        })}
+      </S.RowWrapper>
+      <Space h="xl"></Space>
+      <Text color="var(--primary-color)" weight="bold" size="lg">
+        재료
+      </Text>
+      <Text>{dummyDidMount.cocktail_ingredients}</Text>
+      <Space h="md"></Space>
+      <Text color="var(--primary-color)" weight="bold" size="lg">
+        믹스방법
+      </Text>
+      <Text>{dummyDidMount.cocktail_description}</Text>
     </>
   );
 };
--- src/pages/CocktionaryDetailPage/style.js
+++ src/pages/CocktionaryDetailPage/style.js
@@ -1,56 +1,18 @@
 import styled from "styled-components";
+import {} from "../../commonStyles";
 // import { Wrapper, Img, Texts, TextBig, TextSmall }
 
 /** Container flex-row (space between) */
-export const TodayCocktailBarHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+
+export const RowWrapper = styled.div`
   width: 100%;
-  margin: 20px 0px;
+  gap: 1.5em;
+`;
+
+export const Inline = styled.div`
+  display: inline-block;
 `;
 
 export const Wrapper = styled.div`
-  height: 100%;
-  margin: 0px auto;
-  margin-top: 20px;
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  flex-wrap: wrap;
-`;
-
-export const Img = styled.img`
-  width: 60%;
-  height: 150px;
-  margin-top: 30px;
-`;
-
-export const Texts = styled.div`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-export const TextBig = styled.div``;
-
-export const TextSmall = styled.div``;
-
-export const CocktailSection = styled.div`
-  margin-top: 20px;
-  width: 45%;
-  height: 240px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-width: 1px;
-  border-color: rgb(176, 176, 176);
-  border-style: solid;
-  border-radius: 10px;
+  width: 100%;
 `;
