b48f8a435be4d59f5159e7905a67bdac9c178914
Henry-Hong <hhr8540@naver.com>
2022-08-19 16:40:40 +0900

    api 테스트용

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  API_ADDRESS: "http://localhost:8000/cocktailbar",
+  // API_ADDRESS: "http://localhost:8000/cocktailbar",
+  API_ADDRESS:
+    "https://port-0-backend-django-73126l6fzbqt1.gksl1.cloudtype.app:8000",
 };
 
 export default config;
